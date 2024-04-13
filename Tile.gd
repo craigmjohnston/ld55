c5ab90702212f5a9d75888f635fb110ed6a347bd
Craig Johnston <craigmjohnston@gmail.com>
2024-04-13 13:46:12 +0100

    Add next tiles to board when clicked
    Closes LD55-6

--- Tile.gd
+++ Tile.gd
@@ -9,13 +9,8 @@ class_name Tile
 @export var suit: TileStats.SUIT
 @export var value: int
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	update_visual()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
 
 func set_stats(suit: TileStats.SUIT, value: int):
 	self.suit = suit
