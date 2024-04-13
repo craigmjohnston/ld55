00ce92c171b3fe070f75ac80fbd30b8108c9c934
Craig Johnston <craigmjohnston@gmail.com>
2024-04-13 08:36:06 +0100

    Tile entity
    Closes LD55-2

---
+++ Tile.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+@onready var icon = $Icon
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print_debug(icon)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
